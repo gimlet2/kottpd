5444c5beef0ea4c97a48df6877ff3cbd496878e3
andrei <andrey.chernishov@gmail.com>
2016-08-22 21:48:20 +0200

    Http status codes were introduced.

--- src/main/kotlin/org/kottpd/Server.kt
+++ src/main/kotlin/org/kottpd/Server.kt
@@ -6,9 +6,10 @@ import java.io.OutputStreamWriter
 import java.io.PrintWriter
 import java.net.ServerSocket
 import java.net.Socket
+import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
-val threadPool = Executors.newCachedThreadPool()
+val threadPool: ExecutorService = Executors.newCachedThreadPool()
 
 val port: Int = (System.getProperty("server.port") ?: "9000").toInt()
 
@@ -49,7 +50,7 @@ class ClientThread(val socket: Socket) : Runnable {
 
 fun doSmth(request: HttpRequest, response: HttpResponse) {
     println(request.content)
-    response.send(200, request.content)
+    response.send(request.content, Status.OK)
 }
 
 data class HttpRequest(val method: HttpMethod,
@@ -62,19 +63,56 @@ data class HttpRequest(val method: HttpMethod,
     }
 }
 
-data class HttpResponse(var status: Int = 200,
+data class HttpResponse(var status: Status = Status.OK,
                         val stream: PrintWriter) {
-    fun send(status: Int, content: String, headers: Map<String, String> = emptyMap()) {
-        stream.println("HTTP1/1 $status OK")
+    fun send(content: String, status: Status = this.status, headers: Map<String, String> = emptyMap()) {
+        stream.println("HTTP1/1 ${status.code} ${status.value}")
         if (headers.isNotEmpty()) {
-            headers.forEach { println("${it.key}: ${it.value}") }
+            headers.forEach { stream.println("${it.key}: ${it.value}") }
             stream.println()
         }
         stream.println(content)
     }
+
 }
 
 enum class HttpMethod {
     GET, POST, PUT, DELETE, OPTIONS, HEAD, TRACE, CONNECT
+}
+
+enum class Status(val code: Int, val value: String) {
+    // 100
+    Continue(100, "Continue"),
+    SwitchingProtocols(101, "Switching Protocols"), Processing(102, "Processing"),
+    // 200
+    OK(200, "OK"),
+    Created(201, "Created"), Accepted(202, "Accepted"), NonAuthoritativeInformation(203, "Non-Authoritative Information"),
+    NoContent(204, "No Content"), ResetContent(205, "Reset Content"), PartialContent(206, "Partial Content"),
+    MultiStatus(207, "Multi-Status"), AlreadyReported(208, "Already Reported"), IMUsed(226, "IM Used"),
+    // 300
+    MultipleChoices(300, "Multiple Choices"),
+    MovedPermanently(301, "Moved Permanently"), Found(302, "Found"),
+    SeeOther(303, "See Other"), NotModified(304, "Not Modified"), UseProxy(305, "Use Proxy"), SwitchProxy(306, "Switch Proxy"),
+    TemporaryRedirect(307, "Temporary Redirect"), PermanentRedirect(308, "Permanent Redirect"),
+    // 400
+    BadRequest(400, "Bad Request"),
+    Unauthorized(401, "Unauthorized"), PaymentRequired(402, "Payment Required"),
+    Forbidden(403, "Forbidden"), NotFound(404, "Not Found"), MethodNotAllowed(405, "Method Not Allowed"),
+    NotAcceptable(406, "Not Acceptable"), ProxyAuthenticationRequired(407, "Proxy Authentication Required"),
+    RequestTimeout(408, "Request Timeout"), Conflict(409, "Conflict"), Gone(410, "Gone"), LengthRequired(411, "Length Required"),
+    PreconditionFailed(412, "Precondition Failed"), PayloadTooLarge(413, "Payload Too Large"), URITooLong(414, "URI Too Long"),
+    UnsupportedMediaType(415, "Unsupported Media Type"), RangeNotSatisfiable(416, "Range Not Satisfiable"),
+    ExpectationFailed(417, "Expectation Failed"), ImATeapot(418, "I'm a teapot"), MisdirectedRequest(421, "Misdirected Request"),
+    UnprocessableEntity(422, "Unprocessable Entity"), Locked(423, "Locked"), FailedDependency(424, "Failed Dependency"),
+    UpgradeRequired(426, "Upgrade Required"), PreconditionRequired(428, "Precondition Required"),
+    TooManyRequests(429, "Too Many Requests"), RequestHeaderFieldsTooLarge(431, "Request Header Fields Too Large"),
+    UnavailableForLegalReasons(451, "Unavailable For Legal Reasons"),
+    // 500
+    InternalServerError(500, "Internal Server Error"),
+    NotImplemented(501, "Not Implemented"), BadGateway(502, "Bad Gateway"),
+    ServiceUnavailable(503, "Service Unavailable"), GatewayTimeout(504, "Gateway Timeout"),
+    HTTPVersionNotSupported(505, "HTTP Version Not Supported"), VariantAlsoNegotiates(506, "Variant Also Negotiates"),
+    InsufficientStorage(507, "Insufficient Storage"), LoopDetected(508, "Loop Detected"), NotExtended(510, "Not Extended"),
+    NetworkAuthenticationRequired(511, "Network Authentication Required")
 }
 
